14c911f7c6b4a2ea9049e730ee2c43c7f15155ec
Viet Nguyen <nguyenhoangviet24022@gmail.com>
2023-08-08 17:45:08 -0500

    Finished 2-player game

--- script.js
+++ script.js
@@ -1,7 +1,86 @@
 let board = document.getElementById('gameBoard-container');
 
+const WinningConditions = [
+    ['00', '01', '02'],
+    ['10', '11', '12'],
+    ['20', '21', '22'],
+    ['00', '10', '20'],
+    ['01', '11', '21'],
+    ['02', '12', '22'],
+    ['00', '11', '22'],
+    ['02', '11', '20']
+];
+
+//the close button that closes the popup
+document.getElementsByClassName('close')[0].addEventListener('click', () => {
+    popup.style.display = "none";
+    restart();
+});
+
+function restart() {
+    let cells = document.querySelectorAll('.cell');
+    cells.forEach(cell => {
+        if (cell.firstChild) cell.removeChild(cell.firstChild);
+        cell.classList.toggle('player2');
+        cell.classList.toggle('player1');
+    })
+    player1.CellsTaken.length = 0;
+    player2.CellsTaken.length = 0;
+    displayPlayer();
+};
+
+function togglePlayer() {
+    let cells = document.querySelectorAll('.cell');
+    cells.forEach(cell => {
+        cell.classList.toggle('player2');
+        cell.classList.toggle('player1');
+    })
+    displayPlayer();
+};
+
+function displayPlayer() {
+    let playerName = document.getElementById('playerName');
+    if (playerName.textContent.localeCompare('Player 1') === 0) {
+        playerName.textContent = 'Player 2';
+    }
+    else {
+        playerName.textContent = 'Player 1';
+    }
+};
+
+function win(cellsTaken) {
+    WinningConditions.forEach(condition => {
+        let count = 0;
+        for (let i = 0; i < condition.length; i++) {
+            if (cellsTaken.includes(condition[i])) count++
+        }
+        if (count === condition.length) {
+            document.getElementById('popup').style.display = "block";
+            let winner = document.getElementById('winner');
+            if (document.querySelector('.player1')) {
+                winner.textContent = 'Player 2'
+            }
+            else {
+                winner.textContent = 'Player 1';
+            }
+        }
+    })  
+};
+
+const Player = (img) => {
+    const CellsTaken = [];
+
+    const takesCell = (cell, bg) => {
+        CellsTaken.push(cell.id);
+        bg.setAttribute('src', img);
+        bg.style.display = 'inline';
+        bg.style.opacity = '0.6';
+    }
+    return {CellsTaken, takesCell};
+};
+
 const gameBoard = (() => {
-    const _gameBoard = [[,,,],[,,,],[,,,]];
+    const gameBoard = [[,,,],[,,,],[,,,]];
     let _board = document.createElement('div');
     _board.setAttribute('class', 'gameBoard');
 
@@ -11,19 +90,31 @@ const gameBoard = (() => {
             return;
         }
         let cell = event.target;
-        let img = document.createElement('img');
+        let bg = document.createElement('img');
         
-        img.setAttribute('src', './icons/pistol.svg');
-        img.style.display = 'inline';
-        cell.appendChild(img);
+        if (event.target.classList.contains('player2')) {
+            player2.takesCell(cell, bg);
+            togglePlayer();
+        }
+        else {
+            player1.takesCell(cell, bg);
+            togglePlayer();
+        }
+        cell.appendChild(bg);
+        if (player1.CellsTaken.length >= 3) {
+            win(player1.CellsTaken);
+        }
+        if (player2.CellsTaken.length >= 3) {
+            win(player2.CellsTaken);    
+        }
     }
 
     const render = () => {
-        for (let i = 0; i < _gameBoard.length; i++) {
-            for (let j = 0; j < _gameBoard[i].length; j++) {
+        for (let i = 0; i < gameBoard.length; i++) {
+            for (let j = 0; j < gameBoard[i].length; j++) {
                 let cell = document.createElement('div');
 
-                cell.setAttribute('id', `cell-${i}${j}`);
+                cell.setAttribute('id', `${i}${j}`);
                 cell.classList.add('cell', 'player1');
                 cell.addEventListener('click', (event) => play(event));
 
@@ -33,31 +124,10 @@ const gameBoard = (() => {
         return _board;
     }                   
     return {
-        render,
+        render
     };                 
 })();
 
-const displayController = (() => {
-
-})();
-
-const Player = () => {
-
-};
-
-function togglePlayer() {
-    let cells = document.querySelectorAll('.cell');
-    cells.forEach(cell => {
-        cell.classList.toggle('player2');
-    })
-
-    let playerName = document.getElementById('playerName');
-    if (playerName.textContent.localeCompare('Player 1') === 0) {
-        playerName.textContent = 'Player 2';
-    }
-    else {
-        playerName.textContent = 'Player 1';
-    }
-}
-
+const player1 = Player('./icons/pistol.svg');
+const player2 = Player('./icons/sword.svg');
 board.appendChild(gameBoard.render());
